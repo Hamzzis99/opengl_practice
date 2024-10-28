dbe10ce011d9c3481a68291043f7e18c0dc65b58
Hamzzis99 <hamzzi99@naver.com>
2024-10-29 07:42:55 +0900

    일단연습
    연습

---
+++ Lecture/Question 15/Question 15/open_obj.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 구조체 정의
+typedef struct {
+    float x, y, z;
+} Vertex;
+
+typedef struct {
+    unsigned int v1, v2, v3;
+} Face;
+
+typedef struct {
+    Vertex* vertices;
+    size_t vertex_count;
+    Face* faces;
+    size_t face_count;
+} Model;
+
+// 함수 선언
+void read_newline(char* str);
+void read_obj_file(const char* filename, Model* model);
